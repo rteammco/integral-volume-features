fc5896a779ab1a89931d0c997f508f69ea5f76dd
Richard Teammco <teammcr192@gmail.com>
2016-01-30 15:12:09 -0600

    Updated the viewer to a more sophisticated version.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,12 @@
 
 #include "voxel_grid.h"
 
+using pcl::PointCloud;
+using pcl::PointXYZ;
+using pcl::visualization::PCL_VISUALIZER_POINT_SIZE;
+using pcl::visualization::PCLVisualizer;
+using pcl::visualization::PointCloudColorHandlerCustom;
+
 
 int main (int argc, char **argv) {
   // Process args.
@@ -31,17 +37,24 @@ int main (int argc, char **argv) {
 
   //const std::string filename = "../data/bun000.ply";
   const std::string filename = "../data/table_scene_lms400.pcd";
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  PointCloud<PointXYZ>::Ptr cloud(new PointCloud<PointXYZ>);
   // Load the file.
   //pcl::PLYReader reader;
   pcl::PCDReader reader;
-  if (reader.read<pcl::PointXYZ>(filename, *cloud) == -1) {
+  if (reader.read<PointXYZ>(filename, *cloud) == -1) {
     PCL_ERROR("Couldn't read file!\n");
     return -1;
   }
   std::cout << "Loaded " << cloud->size() << " points." << std::endl;
-  pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
-  viewer.showCloud(cloud);
-  while (!viewer.wasStopped()) {}
+  // Load the PCL 3D visualization window.
+  PCLVisualizer viewer("3D Viewer");
+  viewer.setBackgroundColor(0, 0, 0);
+  PointCloudColorHandlerCustom<PointXYZ> single_color(cloud, 0, 255, 0);
+  viewer.addPointCloud<PointXYZ>(cloud, single_color, "Point Cloud");
+  viewer.setPointCloudRenderingProperties(
+      PCL_VISUALIZER_POINT_SIZE, 1, "Point Cloud");
+  viewer.addCoordinateSystem(1.0);
+  viewer.initCameraParameters();
+  viewer.spin();
   return 0;
 }
