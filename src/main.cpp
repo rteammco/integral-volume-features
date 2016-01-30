e09f54bb13c1a54effc5508c40140182df7cdd63
Richard Teammco <teammcr192@gmail.com>
2016-01-30 10:47:12 -0600

    Added sets data to experiment with.

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,23 @@
 #include <string>
 
 #include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
 #include <pcl/point_types.h>
 #include <pcl/visualization/cloud_viewer.h>
 
 
 int main (int argc, char **argv) {
-  const std::string filename = "../data/example.pcd";
+  //const std::string filename = "../data/bun000.ply";
+  const std::string filename = "../data/table_scene_lms400.pcd";
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
   // Load the file.
-  if (pcl::io::loadPCDFile<pcl::PointXYZ>(filename, *cloud) == -1) {
-    PCL_ERROR("Couldn't read file test_pcd.pcd \n");
+  //pcl::PLYReader reader;
+  pcl::PCDReader reader;
+  if (reader.read<pcl::PointXYZ>(filename, *cloud) == -1) {
+    PCL_ERROR("Couldn't read file!\n");
     return -1;
   }
+  std::cout << "Loaded " << cloud->size() << " points." << std::endl;
   pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
   viewer.showCloud(cloud);
   while (!viewer.wasStopped()) {}
