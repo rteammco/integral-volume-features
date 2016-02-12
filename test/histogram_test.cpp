93e335294d1cdc8b02cf9a80172bbae2050fd053
Richard Teammco <teammcr192@gmail.com>
2016-02-11 21:45:08 -0600

    Fixed CMake and test demo to integrate GTests.
    GTests now work (cleaning up and actual tests needed).

--- test/histogram_test.cpp
+++ test/histogram_test.cpp
@@ -1,7 +1,12 @@
+#include <vector>
+
 #include "histogram.cpp"
+
 #include <gtest/gtest.h>
 
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+
+TEST(HistogramTest, ExampleTest) {
+  std::vector<float> vals = {0.1, 0.5, 1.2};
+  iv_descriptor::Histogram h(vals);
+  EXPECT_EQ(1, h.GetRareValues(0.1).size());
 }
